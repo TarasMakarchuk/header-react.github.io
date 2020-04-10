52731fa945e9cc3edab5bc75ae29ac4ea667b1a7
TarasMakarchuk <43746781+TarasMakarchuk@users.noreply.github.com>
2020-04-10 13:48:59 +0300

    Add files via upload

---
+++ precache-manifest.b77e7f39c15a8ea57c05a97f571a9ee3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8469e0da86db1294656b0253980f9a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "966151b8ad66186439f1",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "336866eabd11cca415bf",
+    "url": "/static/css/main.bc9702fb.chunk.css"
+  },
+  {
+    "revision": "966151b8ad66186439f1",
+    "url": "/static/js/2.52ca7625.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.52ca7625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336866eabd11cca415bf",
+    "url": "/static/js/main.6b68e4f7.chunk.js"
+  },
+  {
+    "revision": "b5cc876fba29b58122bc",
+    "url": "/static/js/runtime-main.92bada2e.js"
+  },
+  {
+    "revision": "c7dce8d0b5228f1b8f3b30bdd30ca42c",
+    "url": "/static/media/user-profile.c7dce8d0.png"
+  }
+]);
